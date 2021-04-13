7ad1bfbbf381e9db524244747a4b2470ddf8dd29
bamidele tewogbade <bishoptewogbade@gmail.com>
2021-04-13 15:00:50 +0000

    Password reset page added

---
+++ reset_password.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Password Reset</title>
+</head>
+<body>
+<form action="reset_process.php" method="post">
+	New Password: <input type="text" name="username" value="" />
+	<br>
+	<br>
+	Confirm Password: <input type="text" name="password" value="" />
+	<br>
+	<br>
+	<input type="submit" name="submit" value="Register">
+</form>
+</body>
+</html>
